501de7715a56e83d165d7fb7c8bd1eabe1687203
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-11-06 17:30:50 +0900

    add CI settings

--- tests/PKU-PosterLayout_test.py
+++ tests/PKU-PosterLayout_test.py
@@ -1,3 +1,5 @@
+import os
+
 import datasets as ds
 import pytest
 
@@ -7,6 +9,13 @@ def dataset_path() -> str:
     return "PKU-PosterLayout.py"
 
 
+@pytest.mark.skipif(
+    condition=bool(os.environ.get("CI", False)),
+    reason=(
+        "Because this loading script downloads a large dataset, "
+        "we will skip running it on CI."
+    ),
+)
 @pytest.mark.parametrize(
     argnames=(
         "expected_num_train",
